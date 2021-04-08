a62261cb65b5862a7048963a6a3e8fc725ad291f
Caleb Odinga <caleb.odinga@student.moringaschool.com>
2021-04-08 16:30:59 +0300

    Add BMI calculating function

--- js/script.js
+++ js/script.js
@@ -15,4 +15,12 @@ function remainder(number1, number2){
 }
 let number1 = parseInt(prompt('Enter the first number'));
 let number2 = parseInt(prompt('Enter the second number'));
-alert(`The sum is ${addition(number1, number2)}`);                                                                                                                                                                                                 
+alert(`The sum is ${addition(number1, number2)}`);/*
+//Calculating BMI. BMI=kg/m2
+let weight=prompt(`Enter your weight in KG`);
+let height= prompt(`Enter your height in Metres`);
+function bmi(weight, height){
+  return weight/(height*height);
+}
+alert(bmi(230,1.6));*/
+
