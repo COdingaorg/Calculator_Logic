6e2970eb916b1e1473ceb0cefd0c8bb58029f829
Caleb Odinga <caleb.odinga@student.moringaschool.com>
2021-04-08 15:42:14 +0300

    Adds functions for addition, subtraction, division, multiplication and remainder to js file also adds prompt to enter number 1 and number2

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function addition(number1, number2){
+  return number1+number2;
+}
+function subtraction(number1, number2){
+  return number1-number2
+}
+function multiplication(number1, number2){
+  return number1*number2
+}
+function division(number1, number2){
+  return number1/number2
+}
+function remainder(number1, number2){
+  return number1%number2
+}
+let number1 = parseInt(prompt('Enter the first number'));
+let number2 = parseInt(prompt('Enter the second number'));
+alert(`The sum is ${addition(number1, number2)}`);                                                                                                                                                                                                 
